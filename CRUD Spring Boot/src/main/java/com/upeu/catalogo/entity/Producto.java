62a6821365bce760f83e41b7a758d5c9f3d07a5a
IVANMAMANI2003 <ivanyomm.2003@gmail.com>
2023-04-24 08:30:05 -0500

    Tablas dependientes

---
+++ CRUD Spring Boot/src/main/java/com/upeu/catalogo/entity/Producto.java
@@ -0,0 +1,26 @@
+package com.upeu.catalogo.entity;
+
+import jakarta.persistence.Entity;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+
+
+@Entity
+@Data
+public class Producto {
+    @Id
+    @GeneratedValue(strategy =GenerationType.IDENTITY)
+    private Integer proid;
+    private String proname;
+    private Double proprecio;
+    private int prostock;
+    
+    @ManyToOne
+    @JoinColumn(name="idCat")
+    private Categoria categoria;
+}
